2adbcafc56da51f8ba8198fceffa071806556e46
Rumel <55288766+mahmudunnabi091@users.noreply.github.com>
2019-09-13 23:19:16 +0600

    Add files via upload

---
+++ Variable0.py
@@ -0,0 +1,13 @@
+name="Rumel"
+age="\'20\'"
+university="Bangladesh University"
+place="Dhaka"
+subject="CSE"
+job="Microsoft or Google"
+print("My name is "+name)
+print("i am", age ,"years old")
+print("i live in "+place)
+print("i am study in "+subject)
+print("i am study at "+university)
+print("i need to work at "+job)
+
